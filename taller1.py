9248b372a8e5f399ed44fa50a009472847d1a586
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-02-17 06:20:40 -0500

    ejercicio7 taller1

--- taller1.py
+++ taller1.py
@@ -1,5 +1,13 @@
-# En sus propias palabras, qué son las funciones
-# preconstruidas y proporcione 2 ejemplos.
+# Cuál es la diferencia entre un condicional simple y un condicional compuesto?
 
-print("Son bloques de codigo reutilizable, contruidos con el fin de de realizar una actividad especifica")
+print("el condicional simple es el que ejecuta una accion cuando se cumple una condicion")
+ojos = "azules"
+if(ojos == "azules"):
+    print("Que bonitos ojos tienes")
 
+print("el condicional compuesto es el que ejecuta una accion uando se cumple una condicion y otra accion diferente si no se cumple la condicion")
+num = 3
+if(num%2 == 0):
+    print("el numero es par")
+else:
+    print("el numero es inpar")
