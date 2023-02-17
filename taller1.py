1d5f488d83fd9e76a6b5d5a0e16c8501e5948480
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-02-17 08:09:20 -0500

    Exercise 4

--- taller1.py
+++ taller1.py
@@ -1,16 +1,36 @@
-# Construya un algoritmo en Python, que permita ingresar la
-# información de un empleado e imprima el nombre, los
-# apellidos y la antigüedad. Los datos que se deben solicitar
-# son los siguientes:
-# *Nombre * Teléfono *Año de ingreso a la empresa
-# *Apellidos *Edad.
+""" 4. Una empresa tiene 500 almacenes. Cada almacén debe
+reportar el nombre y 5 registros c/u, contiene el tipo de articulo
+y el número de unidades vendidas de ese artículo.
 
-nombre = input("Digite su nombre ")
-apellido = input("Digite su apellido ")
-telefono = int(input("Digite su numero de telefono "))
-edad = int(input("Digite su edad "))
-año_ingreso = int(input("Digite el año en que ingreso a la empresa "))
-año_actual = 2023
+Haga un programa en Python para determinar cuál fue el
+almacén que mas vendió, cual fue el articulo más vendido de
+ese almacén y cual el más vendido en general.
+ """
+cant_almacenes = 2
+articulos = {}
+cant_articulos_vendidos_total = 0
+nombre_almacen = ""
+articulo_mas_vendido = 0
+articulo_mas_vendido_almacen = ""
 
-print(f"{nombre.upper()} {apellido.upper()} tienes una antiguedad en la empresa de {año_ingreso - año_actual} años")
+for i in range(cant_almacenes):
+    name = input(f"Digite el nombre del almacen {i+1}: ")
+    sum = 0
+    for b in range(2):
+        articulo = input(f"Ingrese el tipo de artículo vendido en el almacén {name}: ")
+        unidades = int(input(f"Ingrese el número de unidades vendidas de {articulo}: "))
+        sum = sum + unidades
+        articulos[articulo] = articulos.get(articulo, 0) + unidades
+    articulo_vendido = max(articulos, key=articulos.get)
+    print(f"\nEl artículo más vendido en el almacen {name} fue {articulo_vendido} con un total de {articulos[articulo_vendido]} unidades vendidas.\n")
+
+    if(sum >= cant_articulos_vendidos_total):
+        nombre_almacen = name
+    
+    if(articulos[articulo_vendido] >= articulo_mas_vendido):
+        articulo_mas_vendido = articulos[articulo_vendido]
+        articulo_mas_vendido_almacen = articulo_vendido
+
+print(f"el almacen que mas vedio fue {nombre_almacen}")
+print(f"el articulo mas vendido en general fue {articulo_mas_vendido_almacen} con una cantidad de {articulo_mas_vendido}")
 
